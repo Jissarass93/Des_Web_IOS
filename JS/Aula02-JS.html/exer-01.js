8f9c4a347aa939ebc40748a2f76b98788581ac94
jissara <jissarasouza@gmail.com>
2022-04-27 21:51:32 -0300

    arquivo js Aula 02

---
+++ JS/Aula02-JS.html/exer-01.js
@@ -0,0 +1,13 @@
+console.log ("calculo imc");
+
+var pessoa = 'Maria'
+var peso = 61 
+var altura = 1.66
+
+var calculo = peso/(altura*altura);
+
+
+const hello = ` ${pessoa}  pesa ${peso} , sua altura é ${altura} e seu imc é ${calculo}`;
+
+
+console.log(hello);
