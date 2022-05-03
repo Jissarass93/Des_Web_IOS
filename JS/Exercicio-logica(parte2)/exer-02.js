34c213c280b6cffab52a52320ac8064986fa8c2a
Jissara <jissarasouza@gmail.com>
2022-05-02 21:25:21 -0300

    arquivos exercícios

---
+++ JS/Exercicio-logica(parte2)/exer-02.js
@@ -0,0 +1,13 @@
+console.log ("reajuste");
+
+var salario = 1000
+var dinheiro = salario. toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
+var percentual = 0.13;
+var aumento = salario * percentual;
+var real= aumento. toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
+var reajuste = salario + aumento;
+var valor = reajuste. toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
+
+
+const hello = `O salário era: ${dinheiro}  ouve um percentual de reajuste no valor de ${real} , e seu valor atual é ${valor} `;
+console.log(hello)
