fca88cf336f1d31326ec066b2bafa021de808dec
jissara <jissarasouza@gmail.com>
2022-05-04 18:15:18 -0300

    exercicio 04 js

--- JS/Aula 04 -arrays/mainexer02.js
+++ JS/Aula 04 -arrays/mainexer02.js
@@ -7,14 +7,15 @@
  objetos.splice(2, 0, "geladeira", "fogão");
  console.log(`ela ganhou uma geladeira e um fogão`)
 
- objetos.splice (2, 0, "Televisão", "televisão" );
+ objetos.splice (2, 0, "televisão", "televisão" );
 console.log(`ela ganhou 2 TV´S`)
 
 
 delete objetos[3]; 
 console.log(`ela doou uma de suas tv´s`);
 
-
+//O sort classifica um array em ordem alfabética
+objetos.sort();
 console.log(` Maria com sua compras e algumas doações ficou com alguns objetos para sua casa que são: ${objetos}.`)
 
 
