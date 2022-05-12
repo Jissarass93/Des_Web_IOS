54246866bc4532fe8917df776274c62fa523d15d
jissara <jissarasouza@gmail.com>
2022-05-12 18:12:25 -0300

    aula 07 js

---
+++ JS/Aula07-Estruturas condicionais/exercicio.js
@@ -0,0 +1,37 @@
+// Criar um array de colaboradores da empresa, com pelo
+// menos 5 pessoas, informando Nome, Salário e vínculo
+// (CLT/PJ) e efetuar o cálculo do valor de INSS de cada
+// colaborador com base na tabela do ano atual.
+// OBS: é necessário ter funcionários nos dois regimes,
+// utilizar if/else para verificar o regime e um if encadeado
+// ou switch/case para verificar as demais condições, no final
+// exibir o nome de cada colaborador com sua contribuição,
+// para o regime PJ exibir o valor zerado.
+
+let colaborador1 = {
+    firstName: 'Maria',
+    Salário: 1212.00,
+    Vínculo: 'CLT'
+}
+let colaborador2 = {
+    firstName: 'Pedro',
+    Salário: 3825.00,
+    Vínculo: 'CLT'
+}
+let colaborador3 = {
+    firstName: 'Ricardo',
+    Salário: 2225.00,
+    Vínculo: 'CLT'
+}
+let colaborador4 = {
+    firstName: Alice,
+    Salário: 4635.00,
+    Vínculo: 'pj'
+}
+let colaborador5 = {
+    firstName: 'Antonio',
+    Salário: 5612.00,
+    Vínculo: 'CLT'
+}
+
+
