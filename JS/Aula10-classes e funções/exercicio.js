4d9800844f2b45109bd90f71f31b115eee9a64a2
jissara <jissarasouza@gmail.com>
2022-05-18 18:57:13 -0300

    ...

--- JS/Aula10-classes e funções/exercicio.js
+++ JS/Aula10-classes e funções/exercicio.js
@@ -23,26 +23,37 @@ class pessoa {
         return idade
     }
 
-    getNiver() {
-        if (dataHoje.getMonth() <= this.dataNascimento.getMonth() && dataHoje.getDate() < this.dataNascimento.getDate()) {
-            return false
-        }
-        return true
-    }
+
+    // getNiver() {
+    //     if (dataHoje.getMonth() <= this.dataNascimento.getMonth() && dataHoje.getDate() < this.dataNascimento.getDate()) {
+    //         return false
+    //     }
+    //     return true
+    // }
 }
 
 const dataHoje = new Date()
 
-let pessoas = new pessoa[10]
-pessoa[0] = 'Joao', new Date(2003, 4, 31)
-pessoa[1] = 'Maria', new Date(1990, 7, 15)
+let pessoas = new Array(10);
+pessoas[0] = new pessoa('Joao', new Date(2003, 4, 31));
+pessoas[1] = new pessoa('Maria', new Date(1990, 7, 15));
+pessoas[2] = new pessoa('Maria', new Date(1930, 6, 18));
+pessoas[3] = new pessoa('Maria', new Date(1993, 4, 25));
+pessoas[4] = new pessoa('Maria', new Date(1998, 8, 1));
+pessoas[5] = new pessoa('Maria', new Date(2000, 11, 5));
+pessoas[6] = new pessoa('Maria', new Date(2004, 12, 3));
+pessoas[7] = new pessoa('Maria', new Date(1980, 1, 9));
+pessoas[8] = new pessoa('Maria', new Date(1991, 2, 8));
+pessoas[9] = new pessoa('Maria', new Date(1960, 10, 29));
 
 
-console.log([].getIdade())
+// console.log(pessoas[1].getIdade())
 
+pessoa.prototype.getNiver = () => {
+    if (dataHoje.getMonth() <= this.dataNascimento.getMonth() && dataHoje.getDate() < this.dataNascimento.getDate()) {
+        return false
+    }
+    return true
+};
 
-
-// pessoa.prototype.getNiver = function () {
-//     return `${this.getNiver}`;
-
-// }
+console.log(pessoas[1].getNiver())
