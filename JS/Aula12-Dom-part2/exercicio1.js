e8ca4fc8fff520a0de563ad930317cd4760192de
unknown <jissarasouza@gmail.com>
2022-05-24 18:32:40 -0300

    atualizando js 12

--- JS/Aula12-Dom-part2/exercicio1.js
+++ JS/Aula12-Dom-part2/exercicio1.js
@@ -1,30 +1,3 @@
-// No arquivo HTML crie três botões.
-// Crie uma classe chamada B_12 essa classe será aplicada em
-// todos os botões.
-// Em cada botão crie um Id o primeiro botão terá o Id de B_01, o
-// segundo botão terá Id B_02 e assim por diante até o último botão.
-// No main.js chame a class dos botões e insira o seguintes estilo:
-// Borda de solid 2px #D9BC66
-// Espaçamento de .5rem
-// cor de fundo #BFBFBF
-// margem de .7rem
-// O primeiro botão terá um evento onclick onde ao clicar será
-// chamada uma função no JS com os seguintes requisitos:
-// Utilizando o DOM insira uma imagem da série Umbrella
-// Academy ao lado do botão e mude a cor da body para #262525;
-// No segundo botão terá um evento onclick onde ao clicar chama
-// uma função no JS com os seguintes requisitos:
-// terá que aparecer um prompt no browser solicitando o nome
-// do usuário e quando o nome for informado o usuário clicar em OK
-// o nome deverá aparecer abaixo do botão com uma mensagem
-// "Olá (nome) Bem-vindo a nossa academia".
-// No terceiro botão terá um evento onclick onde ao clicar chama
-// uma função no JS com os seguintes requisitos:
-// No browser deve aparecer um prompt perguntando um
-// número para o usuário logo após informar o número ele deve
-// retornar abaixo do botão a tabuada do número informado.
-// Exemplo de retorno:
-
 
 titulo.innerHTML = 'Exercício DOM-parte-2';
 
