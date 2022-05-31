ae0eede12a301107f0679518cecedd8b34c5d08a
Jissara <jissarasouza@gmail.com>
2022-05-31 16:20:37 -0300

    exercicio 13 incomp js

---
+++ JS/Aula13-DOM-part3/exercício/exercicio.js
@@ -0,0 +1,80 @@
+// Você tem o objetivo de refatorar o seu código do exercicio da
+// Aula JS 12 - JavaScript HTML DOM – Parte 02 apenas
+// utilizando QuerySelector
+// Será adicionado mais algumas validações;
+// No botão que inseri a imagem válida para que não seja
+// possível inserir mais de uma imagem ao clicar no botão.
+// Por último crie um botão que remova todo o conteúdo da sua
+// página web apenas deixando os botões amostra.
+
+
+titulo.innerHTML = 'Exercício DOM-parte-2';
+
+var btns = document.querySelector('B_12');
+var btn1 = document.querySelector('B_01');
+var btn2 = document.querySelector('B_02');
+var btn3 = document.querySelector('B_03')
+var btn4 = document.querySelector('B_04')
+
+
+for (let i = 0; i < btns.length; i++) {
+    btns[i].style.border = '2px solid #D9BC66';
+    btns[i].style.padding = '.5rem';
+    btns[i].style.margin = '.7rem';
+    btns[i].style.backgroundColor = '#D9BC66';
+
+}
+
+
+function display_image(src) {
+    var a = document.createElement("img");
+    a.src = './img/umbrella-academy.jpeg';
+    document.body.style.backgroundColor = '#262525'
+    document.body.style.color = 'white'
+    document.body.appendChild(a);
+    
+}
+    
+
+btn1.addEventListener('', display_image(a.src))
+
+
+
+function display_nome() {
+    var usuario = prompt('Escreva seu nome');
+    var titulo = document.createElement('h2');
+    titulo.innerText = 'Seu nome é';
+    var nome = document.createElement('p');
+    nome.innerText = `Olá ${usuario} Bem-vindo a nossa academia`;
+    document.body.appendChild(titulo)
+    document.body.appendChild(nome)
+}
+
+
+function display_numero() {
+
+
+    var numero = prompt('coloque um numero');
+    numero = parseInt(numero)
+
+    if (isNaN(numero)) {
+        alert('ERRO coloque um valor numerico')
+        return
+    }
+
+    var titulo = document.createElement('h2');
+    titulo.innerText = 'Tabuada';
+    document.body.appendChild(titulo)
+
+    for (let contador = 1; contador <= 10; contador++) {
+        var soma = document.createElement('p')
+        soma.innerText = `${numero} X ${contador} = ${numero * contador}`
+        document.body.appendChild(soma)
+    }
+
+
+}
+function display_reset() {
+    
+reset
+}
--- JS/Aula15-DOM-part5/exercício/exercicio.js
+++ JS/Aula15-DOM-part5/exercício/exercicio.js
@@ -1,60 +1,79 @@
-let form = document.getElementById('addForm');
-let itemList = document.getElementById('items');
-let filter = document.getElementById('filter');
+// let form = document.getElementById('addForm');
+// let itemList = document.getElementById('items');
+// let filter = document.getElementById('filter');
 
-// Form submit event
-form.addEventListener('submit', addItem);
-// Delete event
-itemList.addEventListener('click', removeItem);
-// Filter event
-filter.addEventListener('keyup', buscarItems);
 
-// Adiciona item
-function addItem(e) {
-    e.preventDefault();
-    // Pega o valor do <input>
-    let newItem = document.getElementById('item').value;
-    // Cria novo elemento <li>
-    let li = document.createElement('li');
-    // Adiciona classe
-    li.className = 'list-group-item';
-    // Adiciona o texto no novo elemento com o valor armazenado no newItem
-    li.appendChild(document.createTextNode(newItem));
-    // Cria o elemento botão para deletar um item
-    let deleteBtn = document.createElement('button');
-    // Adiciona classes para o botão de deletar
-    activeBtn.className = 'btn btn-sucess btn-sm float-end active';
-    // Acrescenta o texto no novo elemento
-    activeBtn.appendChild(document.createTextNode('img'));
-    // Acrescenta o botão no elemento li
-    li.appendChild(activeBtn);
-    itemList.appendChild(li); // Adiciona o novo item na lista
-    form.reset(); // Limpa o formulário
+// form.addEventListener('submit', addItem);
+// itemList.addEventListener('click', removeItem);
+// filter.addEventListener('keyup', buscarItems);
+
+// function addItem(e) {
+//     e.preventDefault();
+//     // Pega o valor do <input>
+//     let newItem = document.getElementById('item').value;
+//     // Cria novo elemento <li>
+//     let li = document.createElement('li');
+//     // Adiciona classe
+//     li.className = 'list-group-item';
+//     // Adiciona o texto no novo elemento com o valor armazenado no newItem
+//     li.appendChild(document.createTextNode(newItem));
+//     // Cria o elemento botão para deletar um item
+//     let deleteBtn = document.createElement('button');
+//     // Adiciona classes para o botão de deletar
+//     deleteBtn.className = 'btn btn-danger btn-sm float-end p-1 delete';
+//     // Acrescenta o texto no novo elemento
+//     deleteBtn.appendChild(document.createTextNode('x'));
+//     // Acrescenta o botão no elemento li
+//     li.appendChild(deleteBtn);
+//     itemList.appendChild(li); // Adiciona o novo item na lista
+//     form.reset(); // Limpa o formulário
+// }
+// function removeItem(e) {
+//     if (e.target.classList.contains('delete')) {
+//         let li = e.target.parentElement;
+//         itemList.removeChild(li);
+//     }
+// }
+
+
+// // Delete event
+// itemList.addEventListener('click', addItem);
+// // Filter event
+// filter.addEventListener('keyup', buscarItems);
+// function adicionaBotao(e) {
+//     e.preventDefault();
+//     // Pega o valor do <input>
+//     let newItem = document.getElementById('item').value;
+//     // Cria novo elemento <li>
+//     let li = document.createElement('li');
+//     // Adiciona classe
+//     li.className = 'list-group-item';
+//     // Adiciona o texto no novo elemento com o valor armazenado no newItem
+//     li.appendChild(document.createTextNode(newItem));
+//     // Cria o elemento botão para adicionar um item
+//     let activeBtn = document.createElement('button');
+//     // Adiciona classes para o botão de deletar
+//     activeBtn.className = 'btn btn-success btn-sm float-end p-1 active';
+//     // Acrescenta o texto no novo elemento
+//     activeBtn.appendChild(document.createTextNode('+'));
+//     // Acrescenta o botão no elemento li
+//     li.appendChild(activeBtn);
+//     itemList.appendChild(li); // Adiciona o novo item na lista
+//     form.reset(); // Limpa o formulário
+// }
+// var frutas = ['Banana', ' Maça', 'Morango', 'Melão','Manga']
+
+
+var quantidadeFrutas = [0, 0, 0, 0, 0, 0, 0, 0]
+const valorFrutas =[1, 5, 2.5, 5, 8, 5, 5, 2.5]
+const nomeFrutas= ["Maçã", "Abacate", "Banana", "Jaca", "Melancia", "Mamão", "Melão", "Caqui"]
+const Botoes= ["btnMaca", "btnAbacate", "btnBanana", "btnJaca", "btnMelancia", "btnMamão", "btnMelão", "btnCaqui"]
+
+function somaUm(varDeArmazenamento){
+varDeArmazenamento = varDeArmazenamento++;
 }
 
-// Função Remove Item
-function removeItem(e) {
-    if (e.target.classList.contains('delete')) {
-        let li = e.target.parentElement;
-        itemList.removeChild(li);
-    }
-}
 
-// Função buscarItems
-function buscarItems(e) {
-    // Converte o texto digitado para minúsculo
-    let text = e.target.value.toLowerCase();
-    // Busca todos os itens
-    let items = itemList.getElementsByTagName('li');
-    // Converte os itens para array
-    Array.from(items).forEach(function (item) {
-        // Busca o primeiro item da lista
-        let itemName = item.firstChild.textContent;
-        // Busca o item na lista que começa com o mesmo texto digitado
-        if (itemName.toLowerCase().indexOf(text) != -1) {
-            item.style.display = 'block'; // exibe o item
-        } else {
-            item.style.display = 'none'; // oculta o item
-        }
-    });
+for (i= 0; i< valorFrutas.length; i++){
+    document.getElementById(botoes[i]).addEventListener("onClick", somaUm(quantidadeFrutas[i])
 }
