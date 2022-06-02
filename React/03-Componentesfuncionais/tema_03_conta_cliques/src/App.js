255bd5875ca252985076840d1b5fb906b18361c7
unknown <jissarasouza@gmail.com>
2022-06-01 21:49:26 -0300

    react aula 03

---
+++ React/03-Componentesfuncionais/tema_03_conta_cliques/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Exemplo from './Components/Exemplo';
+import Header from './Components/Header';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header nome="Jissara" />
+      <Exemplo />
+    </div>
+  );
+}
+
+export default App; 
