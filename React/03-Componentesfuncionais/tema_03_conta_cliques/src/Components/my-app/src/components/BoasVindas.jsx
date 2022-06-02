7ff86a76f7ee4a5e2a88c81d44784cf3ba22ad50
Jissara <jissarasouza@gmail.com>
2022-06-02 13:54:02 -0300

    arquivo exercicio

---
+++ React/03-Componentesfuncionais/tema_03_conta_cliques/src/Components/my-app/src/components/BoasVindas.jsx
@@ -0,0 +1,18 @@
+import BoasVindas from '../components/my-';
+
+const boasVindas = () => {
+   
+
+    return (
+    
+    <section style={color }>
+        <h1>Seja bem vindo</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia quo, consectetur harum deleniti officia 
+            cum pariatur non. Sit doloribus inventore, voluptatibus velit magni vel consectetur quisquam, quod ut non beatae.</p>
+        
+        </section>
+        
+    );
+};
+
+export default boasVindas
