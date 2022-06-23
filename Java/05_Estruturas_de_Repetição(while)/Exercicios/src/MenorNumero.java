942c35471e5da0b6bda577850d11ecedac1df2d9
Jissara <jissarasouza@gmail.com>
2022-06-23 13:24:24 -0300

    exercicios java aula 05

--- Java/05_Estruturas_de_Repetição(while)/Exercicios/src/MenorNumero.java
+++ Java/05_Estruturas_de_Repetição(while)/Exercicios/src/MenorNumero.java
@@ -2,14 +2,28 @@ import java.util.Scanner;
 
 public class MenorNumero {
     public static void main(String[] args) {
-        System.out.println("Digite um numero");
-        Scanner x = new Scanner(System.in);
-        int numero = x.nextInt();
-        int valor = 0 ;
-        while (valor < 10){
-            int atualValor= x.nextInt();
-            valor ++;
-            if(atualValor<numero)
+        float menor,
+                num;
+        int contador = 2; //contador com o valor '2', para que ele possa contar do número 2 até o 10, dentro do laço while.
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Digite 10 números ");
+        System.out.print("Numero 1: ");
+        num = entrada.nextFloat();
+        menor = num;
+
+        while (contador <= 10) {
+            System.out.print("Numero " + contador + ": ");
+            num = entrada.nextFloat();
+
+            if (num < menor) {
+                menor = num;
+            }
+
+            contador++;
         }
+
+        System.out.println("O menor número digitado é: " + menor);
+        entrada.close();
     }
 }
+
