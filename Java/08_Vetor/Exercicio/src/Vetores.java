3d49e3fa81748c444aa217926974ae6ff3fa4c3f
Jissara <jissarasouza@gmail.com>
2022-06-29 13:02:34 -0300

    exercicio 08 java

--- Java/08_Vetor/Exercicio/src/Vetores.java
+++ Java/08_Vetor/Exercicio/src/Vetores.java
@@ -4,29 +4,30 @@ public class Vetores {
     public static void main(String[] args) {
         double[] A = new double[4];
         double[] B = new double[7];
+        double[] C = new double[11];
 
         Scanner entrada = new Scanner(System.in);
 
-         double tamanho = A.length;
-         double percorre= B.length;
+        int tamanho = C.length;
+        for (int i = 0; i < 4; i++) {
+            C[i] = A[i];
+        }
+
+        for (int i = 10; i < 10; i++) {
+            C[i] = B[i];
+        }
 
         for (int i = 0; i < tamanho; i++) {
             System.out.println(" Digite a nota " + (i + 1));
-            A[i] = entrada.nextDouble();
-        }
-        for (int i = 0; i < percorre; i++) {
-            System.out.println(" Digite + nota " + (i + 5));
-            B[i] = entrada.nextDouble();
-        }
-        for (int i = 0; i < tamanho; i++) {
-            System.out.println(" nota : " + (i + 1) + " = " + A[i]);
-        }
+            C[i] = entrada.nextDouble();
 
-        for (int j = 0; j < percorre; j++) {
-            System.out.println(" nota : " + (j + 5) + " = " + B[j]);
+        }
+        for (int j = 0; j < tamanho; j++) {
+            System.out.println(" nota : " + (j + 1) + " = " + C[j]);
         }
         entrada.close();
     }
 }
+
 
 
