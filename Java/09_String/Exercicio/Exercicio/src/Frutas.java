7a9e1978c934186635d4d472b97726c19ca267af
jissara <jissarasouza@gmail.com>
2022-06-30 21:32:23 -0300

    aula exercicio 09 java

--- Java/09_String/Exercicio/Exercicio/src/Frutas.java
+++ Java/09_String/Exercicio/Exercicio/src/Frutas.java
@@ -1,29 +1,45 @@
+import java.util.Locale;
 import java.util.Scanner;
-import java.util.ArrayList;
+
 public class Frutas {
     public static void main(String[] args) {
-
-
         Scanner entrada = new Scanner(System.in);
+
         String[] frutas = new String[5];
-        String[] vazia = new String[5];
-        int numeroDeTextos = 5;
 
         for (int i = 0; i < 5; i++) {
-            System.out.println("Digite uma fruta " + (i+1)+" :");
-            frutas[i] = entrada.next();
+            System.out.println("Digite uma fruta " + (i + 1) + " :");
+            String vazia = entrada.nextLine();
+            boolean adiciona = true;
+            //verificar se a string está vazia ou se contem pelo menos 3 letras
+
+            for (int j = 0; j < 5; j++) {
+                if (frutas[j] == null) {
+                    continue;
+                }
+
+                if (vazia.compareTo(frutas[j]) == 0 || vazia.isEmpty() || vazia.length() < 3) {
+                    System.out.println("Conteudo inválido");
+                    adiciona = false;
+                    break;
+                }
+            }
+            if (adiciona) {
+                frutas[i] = vazia;
+            }
         }
 
-        for (int i = 0; i < frutas.length; i++)
-        {
-            System.out.println(frutas[i]);
-        }
+        for (int i = 0; i < frutas.length; i++) {
+            if (frutas[i] != null) {
+                System.out.print(frutas[i].toLowerCase()+ " ");
+                System.out.print(frutas[i].toUpperCase() + " ");
+                System.out.println(" ");
 
-
-
+            }
+            entrada.close();
         }
     }
-
+}
 
 
 
---
+++ Java/10_Matriz/Exercicios/src/Matriz4.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Matriz4 {
+    Scanner teclado = new Scanner(System.in);
+
+    int[][] numeros = new int[4][4];
+    int somaLinhas = 0, total = 0;
+}
